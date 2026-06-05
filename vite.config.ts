5468fc7f2f966952b67e8825e2a6977b6c2f2893
Woldhrd <affanabdghafar@gmail.com>
2026-06-05 09:17:01 +0700

    Ubah output build ke folder docs

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
   base   : '/Biodata-Saya/',
+  build  : {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
